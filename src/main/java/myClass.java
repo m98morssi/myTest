44a84782296511c7bce5170350cc6aa13838a927
m98morssi <drumout@gmail.com@gmail.com>
2015-06-03 21:37:50 -0500

    lkhdsfhahf

---
+++ src/main/java/myClass.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+/**
+ * Created by morssi on 6/3/15.
+ */
+public class myClass {
+@Test
+    public void myTest(){
+    System.out.println("kkkkkkkkkkkkkkkkkkkkkkkkkkk");
+}
+}
